b590e15ffe898664a6123de405c74f374fb4d5d3
shiro-han <shan99business@gmail.com>
2020-06-03 11:57:41 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+
+class School
+    attr_accessor :roster
+    attr_reader :name
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if !@roster[grade]
+            @roster[grade] = []
+        end
+        @roster[grade] << student
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        output = @roster.map do |grade, s_array|
+            [grade, s_array.sort]
+        end
+        output.to_h
+    end
+end
