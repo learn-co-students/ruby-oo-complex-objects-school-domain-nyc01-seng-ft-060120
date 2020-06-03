4a2c1329892d6d70a29e694bdb84bf695bb7b957
berellevy <berellevy@gmail.com>
2020-06-03 10:51:50 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require "pry"
+
+class School
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+
+  def add_student(student, age)
+    @roster[age] = [] unless @roster[age]
+    @roster[age] << student
+  end
+
+  def grade(age)
+    @roster[age]
+  end
+  def sort
+    sorted_students = {}
+    @roster.each {|k, v_array| sorted_students[k] = v_array.sort}
+    Hash[sorted_students.sort]
+  end
+end
